9233efe4c362156f6d370af20fa68661c99da12c
KnackPR <TailBeast113@gmail.com>
2023-08-28 16:09:47 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,41 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Footer } from "@/components/Footer";
+import { Button, Container, Divider, Group, MantineProvider, Pagination, Rating, Text, Textarea, Title } from "@mantine/core";
 
 export default function FoodReviewPage() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
-
-      <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+      <Title order={4} mt="sm">Your rating</Title>
+      <Rating defaultValue={0} size='lg' />
+      <Textarea
+        mt="xs"
+        placeholder="Do you enjoy eating?"
+        label="Your review"
+        minRows={3}
+      />
+      <Button color='orange' mt='xs'>Submit Review</Button>
+      <Divider my='sm' />
+      <Group position="center">
+        <Title order={4}>
+          Elon Musk
+        </Title>
+        <Rating defaultValue={5} />
+      </Group>
+      <Text align="center" color="dimmed">Best pizza in this world. I give you X score.</Text>
+      <Divider my='sm' />
+      <Group position="center">
+        <Title order={4}>
+          Mark Zuck
+        </Title>
+        <Rating defaultValue={4} />
+      </Group>
+      <Text align="center" color="dimmed">My favourite part is pepperoni</Text>
+      <Pagination position="center" mt='md' mb="xs" total={20} color="orange" />
+      <Text align="center" color="dimmed" my='sm'>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Footer />
+        </MantineProvider>
       </Text>
     </Container>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div>
-      <p>Copyright ©</p>
+      Copyright © 2023 Peerawich Rattanahiran 650610794
     </div>
   );
 };
